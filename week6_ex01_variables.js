b964af050dc40c31a956f9ba508cca5b6a412248
Kiara Borens <kiaraborens11@gmail.com>
2026-05-19 12:51:47 +0200

    Add files via upload

---
+++ week6_ex01_variables.js
@@ -0,0 +1,20 @@
+// Create a variable called firstName and store your first name in it
+const firstName = "Kiara";
+
+// Create a variable called lastName and store your last name in it
+const lastName = "Borens";
+
+// Create a variable called age using let (you'll modify this later)
+let age = 18;
+
+// Create a fullName variable that combines firstName and lastName
+const fullName = firstName + " " + lastName;
+
+// Print a greeting to the console using these variables
+console.log("Hello, my name is " + fullName + " and I am " + age + " years old.");
+
+// Try changing the age variable to a different value
+age =19;
+
+// Print the updated age
+console.log("Next year I will be " + age + " years old.");
